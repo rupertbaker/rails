31dc4895a4a562b70e0514d1af9474b5251e1db9
Rupert Baker <rupert.baker@gmail.com>
2022-11-30 11:39:21 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << 'railway.app'
+  config.hosts << "rails-production-fd5c.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
